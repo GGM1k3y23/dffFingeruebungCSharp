0b9849f04fb14a7fa5cefcc5a6f7a5f48da9610b
GGM1k3y23 <31240107+GGM1k3y23@users.noreply.github.com>
2017-08-22 13:35:45 +0200

    GUI und erste Logik entworfen
    -GUI entworfen
    -Logik um Spieler X und O mit dem Spielfeld interagieren zu lassen
    
    Next:
    -entwerfen eines Algorithmus zum ermitteln des Siegers
    -entwicklung eines Einzelspieler Modus

--- dffFingeruebungCSharp/dffFingeruebungCSharp/Form1.Designer.cs
+++ dffFingeruebungCSharp/dffFingeruebungCSharp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace dffFingeruebungCSharp
 {
-    partial class Form1
+    partial class frmSpielfeld
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -28,12 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRunde = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 60);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 60);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(194, 59);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 60);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(34, 129);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(60, 60);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(114, 129);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(60, 60);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(194, 129);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(60, 60);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(34, 199);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(60, 60);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(114, 199);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(60, 60);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(194, 199);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(60, 60);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 47);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Tic Tac Toe";
+            // 
+            // lblRunde
+            // 
+            this.lblRunde.AutoSize = true;
+            this.lblRunde.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRunde.Location = new System.Drawing.Point(315, 59);
+            this.lblRunde.Name = "lblRunde";
+            this.lblRunde.Size = new System.Drawing.Size(134, 24);
+            this.lblRunde.TabIndex = 10;
+            // 
+            // frmSpielfeld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(507, 319);
+            this.Controls.Add(this.lblRunde);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "frmSpielfeld";
+            this.Text = "dff Solutions Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRunde;
     }
 }
 
--- dffFingeruebungCSharp/dffFingeruebungCSharp/Form1.cs
+++ dffFingeruebungCSharp/dffFingeruebungCSharp/Form1.cs
@@ -10,11 +10,41 @@ using System.Windows.Forms;
 
 namespace dffFingeruebungCSharp
 {
-    public partial class Form1 : Form
+    public partial class frmSpielfeld : Form
     {
-        public Form1()
+        string spieler = "X";
+
+        public frmSpielfeld()
         {
             InitializeComponent();
+
+            
+            //Da jeder Button die gleichen Aktionen durchführen soll,
+            //können auch alle die selbe Funktion beim Click Ereignis aufrufen.
+            //Der Code wird dadurch wesentlich kürzer und übersichtlicher
+             
+            button1.Click += Button_Click;
+            button2.Click += Button_Click;
+            button3.Click += Button_Click;
+            button4.Click += Button_Click;
+            button5.Click += Button_Click;
+            button6.Click += Button_Click;
+            button7.Click += Button_Click;
+            button8.Click += Button_Click;
+            button9.Click += Button_Click;
+
+            lblRunde.Text = String.Format("Du bist dran, {0}", spieler);
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            var clickedButton = (Button)sender;
+
+            clickedButton.Text = spieler;
+            spieler = spieler == "X" ? "O" : "X";
+            lblRunde.Text = String.Format("Du bist dran, {0}", spieler);
+
+            // MessageBox.Show("Click durch: " + clickedButton.Name.ToString() );
         }
     }
 }
